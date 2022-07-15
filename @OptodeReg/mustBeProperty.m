d9080aa54bbc55f425cc69d79f552a6ff5ebe0bd
f-k-s <koschumacher@gmx.de>
2022-07-15 23:32:08 +0200

    minor changes for optode revision

--- @OptodeReg/mustBeProperty.m
+++ @OptodeReg/mustBeProperty.m
@@ -2,7 +2,7 @@ function mustBeProperty(p,cn)
 
 k = ismember(p,properties(cn));
 if ~all(k)
-    eidType = 'OptodeReg:InvalidPropertyValue';
+    eidType = sprintf('%s:InvalidPropertyValue',cn);
     notProp = strjoin(p(~k),', ');
     msgType = sprintf('%s is not a class property!',notProp);
     throwAsCaller(MException(eidType,msgType));
